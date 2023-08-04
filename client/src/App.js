6ba2ff3ab490067d43aea6b3ee63d124eb12774e
Luis Mercado <luis.mercado@sleepnumber.com>
2023-08-04 10:38:50 -0400

    SNCOM-717 initial commit

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    message: ''
+  }
+  componentDidMount() {
+    this.fetchMessage()
+  }
+  fetchMessage = async () => {
+    try {
+      const response = await fetch(process.env.REACT_APP_SERVER_URL)
+      const { message } = await response.json()
+      this.setState({ message })
+    } catch (error) {
+      // handle error
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <h1>Hello World</h1>
+            {this.state.message}
+          </p>
+          <p>Version: 5</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
